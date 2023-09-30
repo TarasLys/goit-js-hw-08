b4229ce2b264d490fcbcffce5d26bad0f7ce8e09
TarasLys <134973755+TarasLys@users.noreply.github.com>
2023-09-30 18:37:00 +0000

    Deploying to gh-pages from @ TarasLys/goit-js-hw-08@5daef41f4305270349934e41816ec72aa1b9d80e 🚀

--- 02-video.553477bd.js
+++
@@ -1,2 +0,0 @@
-console.log("OK"),console.log("OK"),console.log("OK");
-//# sourceMappingURL=02-video.553477bd.js.map
--- 02-video.6ca175dc.js
+++
@@ -1,2 +0,0 @@
-console.log("OK"),console.log("OK"),console.log("OK");
-//# sourceMappingURL=02-video.6ca175dc.js.map
