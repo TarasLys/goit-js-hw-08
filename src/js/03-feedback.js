5ee97cd6020c74f16b0b42d059fc0147c452fa2a
TarasLys <opros0609@gmail.com>
2023-10-03 16:18:55 +0300

    seven

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,42 +1,4 @@
-import throttle from 'lodash.throttle';
 
-const formReg = document.querySelector('.feedback-form')
-
-const STORAGE_KEY = "feedback-form-state";
-
-let formData = {};
-
-function onInputHandler(event) { 
-    const inputValue = event.target.value.trim();
-    const inputName = event.target.name;
-    formData[inputName] = inputName;
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-formReg.addEventListener("input", throttle(onInputHandler, 500));
-formReg.addEventListener("submit", (event) => {
-    event.preventDefault();
-   
-    console.log(formData);
-    formData = {};
-    localStorage.removeItem(STORAGE_KEY);
-    event.target.reset();
-});
-
-function refreshForm() { 
-    try {
-        const savedData = localStorage.getItem(STORAGE_KEY);
-        if (!savedData) return;
-        formData = JSON.parse(savedData);
-        Object.entries(formData).forEach(([key, val]) => {
-            formReg.elements[key].value = val;
-        });
-    } catch ({ message }) { 
-        console.log(message);
-    }
-}
-refreshForm();
 
 
 
